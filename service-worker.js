83c5746431e289b4b75f9d4b76766512067adf53
cudnisvemir1992 <digitalbridge.space@gmail.com>
2026-08-24 10:28:18 +0200

    Add Moj balans service worker

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE='moj-balans-v1';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).then(r=>{
+      const copy=r.clone();
+      caches.open(CACHE).then(c=>c.put('./index.html',copy));
+      return r;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{
+    const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;
+  })));
+});
